f94e2e3d684be7cb6e715933ec0f8cd34f1e2e25
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-07-12 12:11:33 +0530

    game-page update

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -18,189 +18,189 @@ import AnimatedScroll from '../AnimatedPage/AnimatedScroll';
 import games from '../../utils/games';
 
 const Home = props => {
-  const {
-    shownGames,
-    cartAmount,
-    cart,
-    cartDisplayed,
-    handleOpenCart,
-    handleCloseCart,
-    clearCart,
-    handleRemoveFromCart,
-    hoverState,
-    setHoverState,
-    overlap,
-    setOverlap,
-    openGamePage
-  } = props;
+     const {
+          shownGames,
+          cartAmount,
+          cart,
+          cartDisplayed,
+          handleOpenCart,
+          handleCloseCart,
+          clearCart,
+          handleRemoveFromCart,
+          hoverState,
+          setHoverState,
+          overlap,
+          setOverlap,
+          openGamePage
+     } = props;
 
-  const [browsing, setBrowsing] = useState(false);
-  const [landingPage, setLandingPage] = useState(true);
+     const [browsing, setBrowsing] = useState(false);
+     const [landingPage, setLandingPage] = useState(true);
 
-  const navigate = useNavigate();
+     const navigate = useNavigate();
 
-  const handleHover = (e) => {
-    let newHoverState = hoverState[e.target.id];
-    newHoverState.hovered = !newHoverState.hovered;
+     const handleHover = (e) => {
+          let newHoverState = hoverState[e.target.id];
+          newHoverState.hovered = !newHoverState.hovered;
 
-    setHoverState([
-        ...hoverState, hoverState[e.target.id] = newHoverState
-    ]);
-  }
+          setHoverState([
+               ...hoverState, hoverState[e.target.id] = newHoverState
+          ]);
+     }
 
-  const handleBrowse = () => {
-    setOverlap(true);
-    setTimeout(() => {
-      setBrowsing(true);
-      navigate('/react-ecommerce-store/browse');
-    }, 1500);
-  }
+     const handleBrowse = () => {
+          setOverlap(true);
+          setTimeout(() => {
+               setBrowsing(true);
+               navigate('/react-ecommerce-store/browse');
+          }, 1500);
+     }
 
-  const handleHome = () => {
-    setBrowsing(false);
-    navigate('/');
-  }
+     const handleHome = () => {
+          setBrowsing(false);
+          navigate('/');
+     }
 
-  const handleNavGamePage = () => {
-    setHoverState([...hoverState, hoverState[21].hovered = false]);
-    navigate('/react-ecommerce-store/games/riseofthetombraider');
-  }
-  
-  const handleNavNotFoundPage = () => {
-    navigate('/react-ecommerce-store/this-page');
-  }
-  
-  const handleNavNotFoundQuery = () => {
-    navigate('/react-ecommerce-store/games/404');
-  }
-  
-  const handlePlayDice = () => {
-    let randomIndex = Math.floor(Math.random() * 32);
-    let randomSurname = games[randomIndex].surname;
-    setOverlap(true);
-    setTimeout(() => {
-      setBrowsing(true);
-      navigate(`/react-ecommerce-store/games/${randomSurname}`);
-    }, 1500);
-  }
+     const handleNavGamePage = () => {
+          setHoverState([...hoverState, hoverState[21].hovered = false]);
+          navigate('/react-ecommerce-store/games/riseofthetombraider');
+     }
 
-  const variants = {
-    hidden: { opacity: 1, x: -150 },
-    visible: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: 150 },
-  }
+     const handleNavNotFoundPage = () => {
+          navigate('/react-ecommerce-store/this-page');
+     }
 
-  const buttonVariants = {
-    hidden: { opacity: 0, y: 900 },
-    visible: { opacity: 1, y: 0, transition: {  y: { type: "tween", duration: 1.5, bounce: 0.3 }} },
-  }
+     const handleNavNotFoundQuery = () => {
+          navigate('/react-ecommerce-store/games/404');
+     }
 
-  return (
-    <div className={styles.main}>
-      {overlap ? 
-          <motion.div 
-            className={styles.overlap}
-            variants={buttonVariants}
-            initial="hidden"
-            animate="visible"
-          >
-    
-          </motion.div> 
-      : null}
+     const handlePlayDice = () => {
+          let randomIndex = Math.floor(Math.random() * 32);
+          let randomSurname = games[randomIndex].surname;
+          setOverlap(true);
+          setTimeout(() => {
+               setBrowsing(true);
+               navigate(`/react-ecommerce-store/games/${randomSurname}`);
+          }, 1500);
+     }
 
-      {cartDisplayed ? <Cart 
-              cartDisplayed={cartDisplayed} 
-              handleOpenCart={handleOpenCart}
-              handleCloseCart={handleCloseCart}
-              cart={cart}
-              cartAmount={cartAmount}
-              handleHover={handleHover}
-              hoverState={hoverState}
-              clearCart={clearCart}
-              handleRemoveFromCart={handleRemoveFromCart}
-              openGamePage={openGamePage}
-      /> : null}
-        <div className={styles.home}>
+     const variants = {
+          hidden: { opacity: 1, x: -150 },
+          visible: { opacity: 1, x: 0 },
+          exit: { opacity: 0, x: 150 },
+     }
 
-                <video autoPlay muted loop className={styles.video}>
-                  <source src={require("../../Resources/image/pyke.mp4")} type="video/mp4" />
-                </video>
+     const buttonVariants = {
+          hidden: { opacity: 0, y: 900 },
+          visible: { opacity: 1, y: 0, transition: { y: { type: "tween", duration: 1.5, bounce: 0.3 } } },
+     }
 
-                <NavBar 
-                  handleHover={handleHover} 
-                  hoverState={hoverState}
-                  browsing={browsing}
-                  handleBrowse={handleBrowse}
-                  handleHome={handleHome}
-                  landingPage={landingPage}
-                  cartAmount={cartAmount}
-                  handleOpenCart={handleOpenCart}
-                  handleCloseCart={handleCloseCart}
-                />
-                <div className={styles.container}>
-                    <div className={styles.left}>
-                        <div className={styles.splash}>
-                          <h1>Game Store</h1>
-                          <p className={styles.intro}>The best destination to buy new games to competitive prices. 24 hour support, "best price" guarantee and a flawless UX. Wish for more? Tell us <span className={styles.here}>below</span> — or check out our <span className={styles.careers}>careers.</span></p>
-                        </div>
-    
-                        <div className={styles.buttons}>
-                              <button className={`${styles.cta} ${styles.browseBtn}`} onClick={handleBrowse} aria-label="Browse">
-                                <Enter className={styles.ctaSVG} />
-                                Browse
-                              </button>
-                              <button className={styles.cta} onClick={handlePlayDice} aria-label="Open random game page">
-                                <Dice className={styles.ctaSVG} />
-                                Play Dice
-                              </button>
-                              <a href="https://github.com/gianlucajahn/react-ecommerce-store" target="_blank"><button className={styles.cta} aria-label="View Repository">
-                                <GitHubLogo className={styles.ctaSVG} />
-                                GitHub
-                              </button></a>
-                              <a href="https://www.linkedin.com/in/gianlucajahn/" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="Open LinkedIn">
-                                <LinkedIn className={`${styles.ctaSVG} ${styles.linkedin}`} />
-                                <span>LinkedIn</span>
-                              </button></a>
-                        </div>
+     return (
+          <div className={styles.main}>
+               {overlap ?
+                    <motion.div
+                         className={styles.overlap}
+                         variants={buttonVariants}
+                         initial="hidden"
+                         animate="visible"
+                    >
+
+                    </motion.div>
+                    : null}
+
+               {cartDisplayed ? <Cart
+                    cartDisplayed={cartDisplayed}
+                    handleOpenCart={handleOpenCart}
+                    handleCloseCart={handleCloseCart}
+                    cart={cart}
+                    cartAmount={cartAmount}
+                    handleHover={handleHover}
+                    hoverState={hoverState}
+                    clearCart={clearCart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    openGamePage={openGamePage}
+               /> : null}
+               <div className={styles.home}>
+
+                    <video autoPlay muted loop className={styles.video}>
+                         <source src={require("../../Resources/image/pyke.mp4")} type="video/mp4" />
+                    </video>
+
+                    <NavBar
+                         handleHover={handleHover}
+                         hoverState={hoverState}
+                         browsing={browsing}
+                         handleBrowse={handleBrowse}
+                         handleHome={handleHome}
+                         landingPage={landingPage}
+                         cartAmount={cartAmount}
+                         handleOpenCart={handleOpenCart}
+                         handleCloseCart={handleCloseCart}
+                    />
+                    <div className={styles.container}>
+                         <div className={styles.left}>
+                              <div className={styles.splash}>
+                                   <h1>Game Store</h1>
+                                   <p className={styles.intro}>The best destination to buy new games to competitive prices. 24 hour support, "best price" guarantee and a flawless UX. Wish for more? Tell us <span className={styles.here}>below</span> — or check out our <span className={styles.careers}>careers.</span></p>
+                              </div>
+
+                              <div className={styles.buttons}>
+                                   <button className={`${styles.cta} ${styles.browseBtn}`} onClick={handleBrowse} aria-label="Browse">
+                                        <Enter className={styles.ctaSVG} />
+                                        Browse
+                                   </button>
+                                   <button className={styles.cta} onClick={handlePlayDice} aria-label="Open random game page">
+                                        <Dice className={styles.ctaSVG} />
+                                        Play Dice
+                                   </button>
+                                   <a href="https://github.com/gianlucajahn/react-ecommerce-store" target="_blank"><button className={styles.cta} aria-label="View Repository">
+                                        <GitHubLogo className={styles.ctaSVG} />
+                                        GitHub
+                                   </button></a>
+                                   <a href="https://www.linkedin.com/in/gianlucajahn/" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="Open LinkedIn">
+                                        <LinkedIn className={`${styles.ctaSVG} ${styles.linkedin}`} />
+                                        <span>LinkedIn</span>
+                                   </button></a>
+                              </div>
+                         </div>
+
+                         <div className={styles.right}>
+                              <div className={styles.buttonsRight}>
+                                   <h2>Quick Navigation</h2>
+                                   <button className={styles.cta} onClick={handleNavGamePage} aria-label="Open a game page">
+                                        <Game className={styles.ctaSVG} />
+                                        Game Page
+                                   </button>
+                                   <button className={styles.cta} onClick={handleNavNotFoundPage} aria-label="Open 404 page">
+                                        <NotFound className={styles.ctaSVG} />
+                                        404 Page
+                                   </button>
+                                   <button className={`${styles.cta} ${styles.lastChild}`} onClick={handleNavNotFoundQuery} aria-label="open 404 query page">
+                                        <NotFoundQuery className={`${styles.ctaSVG}`} />
+                                        404 Query
+                                   </button>
+                                   <a href='https://github.com/gianlucajahn/react-ecommerce-store/commits/main' target="_blank"><button className={styles.cta} aria-label="Open commit log">
+                                        <Git className={styles.ctaSVG} />
+                                        Commit Log
+                                   </button></a>
+                                   <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#performance" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="Open performance test results">
+                                        <Performance className={`${styles.ctaSVG}`} />
+                                        Performance
+                                   </button></a>
+                                   <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#technologies-used" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="View technologies used">
+                                        <img className={styles.technologies} src={require("../../Resources/image/whatruns.png")} alt="WhatRuns logo" />
+                                        Technologies
+                                   </button></a>
+                                   <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#sources" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="View Sources">
+                                        <Sources className={`${styles.ctaSVG}`} />
+                                        Our Sources
+                                   </button></a>
+                              </div>
+                         </div>
                     </div>
-    
-                    <div className={styles.right}>
-                        <div className={styles.buttonsRight}>
-                            <h2>Quick Navigation</h2>
-                            <button className={styles.cta} onClick={handleNavGamePage} aria-label="Open a game page">
-                              <Game className={styles.ctaSVG} />
-                              Game Page
-                            </button>
-                            <button className={styles.cta} onClick={handleNavNotFoundPage} aria-label="Open 404 page">
-                              <NotFound className={styles.ctaSVG} />
-                              404 Page
-                            </button>
-                            <button className={`${styles.cta} ${styles.lastChild}`} onClick={handleNavNotFoundQuery} aria-label="open 404 query page">
-                              <NotFoundQuery className={`${styles.ctaSVG}`} />
-                              404 Query
-                            </button>
-                            <a href='https://github.com/gianlucajahn/react-ecommerce-store/commits/main' target="_blank"><button className={styles.cta} aria-label="Open commit log">
-                              <Git className={styles.ctaSVG} />
-                              Commit Log
-                            </button></a>
-                            <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#performance" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="Open performance test results">
-                              <Performance className={`${styles.ctaSVG}`} />
-                              Performance
-                            </button></a>
-                            <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#technologies-used" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="View technologies used"> 
-                              <img className={styles.technologies} src={require("../../Resources/image/whatruns.png")} alt="WhatRuns logo"/>
-                              Technologies
-                            </button></a>
-                            <a href="https://github.com/gianlucajahn/react-ecommerce-store/blob/main/README.md#sources" target="_blank"><button className={`${styles.cta} ${styles.lastChild}`} aria-label="View Sources">
-                              <Sources className={`${styles.ctaSVG}`} />
-                              Our Sources
-                            </button></a>
-                        </div>
-                    </div>
-                </div>
-        </div>
-    </div>
-  );
+               </div>
+          </div>
+     );
 }
 
 export default Home;
